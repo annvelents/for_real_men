d48bfde79f9c52c379175c4567fd9bafbcb7ecc6
Daria Andreevna <karazayeva.darya@gmail.com>
2015-02-23 16:18:01 +0300

    add man

--- db/migrate/20150223120520_create_timer.rb
+++
@@ -1,8 +0,0 @@
-class CreateTimer < ActiveRecord::Migration
-  def change
-    create_table :timers do |t|
-      t.string :command
-      t.string :time
-    end
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150223120520) do
+ActiveRecord::Schema.define(version: 0) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "timers", force: :cascade do |t|
-    t.string "command"
-    t.string "time"
-  end
 
 end
